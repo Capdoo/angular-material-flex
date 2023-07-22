e30f93c9bd3686c8ce89a9173d4e1f95aa5cf38d
Capdoo <mazher_2014@hotmail.com>
2023-07-22 12:20:10 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+
+    FlexLayoutModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
